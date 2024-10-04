ba4c129d660c1e994beafa7140b5f451b0d6ee85
Taewoo <confort28@gmail.com>
2024-10-04 16:11:37 +0900

    def order changed

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 app = FastAPI()
 
-
 # 키워드와 제목 리스트를 받는 모델 정의
 class NewsRequest(BaseModel):
     keyword: str
@@ -36,7 +35,6 @@ class NewsRequest(BaseModel):
 
 class NewsSummary(BaseModel):
     content: str
-
 
 class NewsData(BaseModel):
     news: str
@@ -58,7 +56,6 @@ lang_model = None
 # 모델 및 토크나이저 로드 함수 (앱 시작 시 실행)
 def load_model_and_tokenizer():
     global model, tokenizer, chatbot_model, chatbot_tokenizer, lang_model
-    # 모델과 토크나이저를 로드합니다.
     tokenizer = tfds.deprecated.text.SubwordTextEncoder.load_from_file('tokenizer')
     model = transformer(
         vocab_size=tokenizer.vocab_size + 2,  # 추가 토큰 크기
@@ -68,15 +65,17 @@ def load_model_and_tokenizer():
         num_heads=2,
         dropout=0.3
     )
-    model.load_weights('transformer(202_0.89_0.22).h5')  # 미리 학습된 가중치 로드
-    
+    model.load_weights('transformer(202_0.89_0.22).h5')
+    lang_model = SentenceTransformer('sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens')
     chatbot_tokenizer = AutoTokenizer.from_pretrained("/root/serving/ModelAPI/stock_chatbot_model")
     chatbot_model = BartForConditionalGeneration.from_pretrained("/root/serving/ModelAPI/stock_chatbot_model")
     chatbot_model.to(device)
     
-    lang_model = SentenceTransformer('sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens')
+# 앱 시작 시 모델과 토크나이저 로드``
+@app.on_event("startup")
+def startup_event():
+    load_model_and_tokenizer()    
     
-
 # API 엔드포인트 구성
 @app.post("/summarizer")
 def summarize_news(news: NewsSummary):
@@ -91,9 +90,7 @@ def keyword_extract(string: NewsData):
     key_text = string.news
     key_text = key_extract_module.preprocessing_article(key_text)
     (article_embedding, n_gram_embeddings, n_gram_words) = key_extract_module.article_embedding(key_text)
-    news_keywords = key_extract_module.max_sum_sim(article_embedding, n_gram_embeddings, n_gram_words, top_n=6,
-                                                   variety=10)
-    
+    news_keywords = key_extract_module.max_sum_sim(article_embedding, n_gram_embeddings, n_gram_words, top_n=6, variety=10)
     (key_embedding, keys_list) = key_extract_module.key_extract(lang_model)
     top_n = 5
 
@@ -105,19 +102,11 @@ def keyword_extract(string: NewsData):
 
     return {"keywords" : news_keywords, "recommand_keywords" : cosine_recommand}
 
-# 앱 시작 시 모델과 토크나이저 로드``
-@app.on_event("startup")
-def startup_event():
-    load_model_and_tokenizer()
-
 @app.post("/news")
 def generate_cloud(news: NewsRequest):
     keyword = news.keyword
     titles = news.titles
-
     news_list = WordCloudService.preprocessing_title(titles)
-
-    # 워드 클라우드 생성
     WordCloudService.to_wordcloud_client(news_list, keyword)
 
 @app.get("/download")
@@ -127,11 +116,9 @@ def download_file():
         return FileResponse(wordcloud_image_path, media_type="image/png")
     return {"error": "word cloud image not found"}
 
-
 @app.post("/chatbot")
 def chatbot_response(question: ChatRequest):
     prompt = f"질문: {question}\n답변: "
-
     input_ids = chatbot_tokenizer.encode(prompt, return_tensors='pt', truncation=True, max_length=128).to(device)
     with torch.no_grad():
         output_ids = chatbot_model.generate(
@@ -158,4 +145,3 @@ def chatbot_response(question: ChatRequest):
         response = "죄송합니다. 이해하지 못했어요."
 
     return json.dumps({"response":response}, ensure_ascii=False).encode('utf8')
-
